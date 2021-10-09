9634d84e59a8082849b7861d7c14e95c178d6d4f
= <=>
2021-10-09 21:03:41 +0530

    remove unncesary code

--- connection.php
+++ connection.php
@@ -3,17 +3,12 @@
     $username = "root";
     $password  = "";
     $dbName = "managment_school";
-
     //create connection
-
     $conn = new mysqli($servername , $username , $password, $dbName);
     //echo "connection succesful";
     //check connection
-
     if($conn -> connect_error)
     {
         die("connection failed .." .$conn -> connect_error);
     }
-
-
 ?>
